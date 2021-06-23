b3bfd32d9b1bd0766955516c938f8a401cc7c5ec
manasd07 <mdwivedi@deqode.com>
2021-06-23 17:58:59 +0530

    feat(local): removed existing dist folder from repo

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-const PORT = 3000;
-app_1.default.listen(PORT, () => {
-    /* tslint:disable-next-line */
-    console.log('Express server listening on http://localhost:3000');
-});
-//# sourceMappingURL=index.js.map
